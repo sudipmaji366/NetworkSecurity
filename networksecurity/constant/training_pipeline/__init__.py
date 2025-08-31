6c18856e43c71c4a743af79a4be7c4639a483c50
sudipmaji366 <sudipmaji366@gmail.com>
2025-08-31 17:03:50 +0530

    ecr repo

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -19,7 +19,7 @@ SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 SAVED_MODEL_DIR =os.path.join("saved_models")
 MODEL_FILE_NAME = "model.pkl"
 
-
+TRAINING_BUCKET_NAME="networksecurity20"
 
 
 """
